7406dd599718e31e8935b2b1c650cb3c6f503191
peng122-byte <870524302@qq.com>
2026-07-31 21:16:47 +0800

    异步低有效复位 + 同步高有效复位 + 使能”的 1 位 D 触发器

---
+++ onebit_D.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+module onebit_D (
+    input  wire clk,
+    input  wire reset,
+    input  wire reset_n,
+    input wire enable,
+    input  wire d,
+    output reg  q
+);
+
+always @(posedge clk or negedge reset_n) begin
+    if ((!reset_n) || (reset))
+        q <= 1'b0;
+    else if(enable)
+        q <= d;
+end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
