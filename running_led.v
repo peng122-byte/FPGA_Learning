cd1ead3e51f0c80dd682802768f6806fcfd36830
peng122-byte <870524302@qq.com>
2026-08-04 17:56:59 +0800

    流水灯增加并行输入功能

--- running_led.v
+++ running_led.v
@@ -3,6 +3,8 @@ module running_led (
     input  wire       reset,
     input  wire       enable,
     input  wire       direction,
+    input wire        P_Flag,
+    input wire  [7:0] P_in,
     output reg  [7:0] led
 );
 
@@ -10,7 +12,9 @@ always @(posedge clk) begin
     if (reset)
         led <= 8'b0000_0001;
     else if (enable) begin
-        if (direction)
+        if (P_Flag)
+        led <= P_in;
+        else if (direction)
             led <= {led[6:0], led[7]};
         else
             led <= {led[0], led[7:1]};
